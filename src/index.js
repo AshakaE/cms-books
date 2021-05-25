7061ac8b26148f7fc9e50c5c11f9ef2fb5f1f0af
AshakaE <ashakaegerega@gmail.com>
2021-05-25 16:10:44 +0100

    Initialize Redux store in index js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import booksReducer from './reducers/books';
+
+const defaultState = {
+    {
+      id: Math.random(Math.random() * 100),
+      title: 'Learn programming the hard way',
+      category: 'Programming',
+    },
+    {
+      id: Math.random(Math.random() * 100),
+      title: 'The Lean Startup',
+      category: 'Novel',
+    },
+};
+
+const store = createStore(booksReducer, defaultState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
