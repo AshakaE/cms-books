221de4fc313ebd3872435eaeeca2b9604517dbaa
helman101 <helmanandresortegon@gmail.com>
2021-05-26 14:55:51 +0200

    implement handleChange in BooksForm container

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BooksForm = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState(''); 
+
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+  const handleChange = (e) => {
+    const element = e.target.localName
+    const newState = e.target.value
+
+    switch(element){
+      case 'input':
+        setTitle(newState);
+        break;
+      default:
+        setCategory(newState);
+        break;
+    };
+  };
 
   return (
     <div>
-      <input type="text" />
-      <select name="booksCategories" id="Books">
+      <input onChange={handleChange} type="text" />
+      <select onChange={handleChange} name="booksCategories" id="Books">
         {categories.map((category) => <option key={category} value={category}>{category}</option>)}
       </select>
       <button type="button">Submit</button>
