7d83109414ba06289558962ff707e76d07c2d09b
helman101 <helmanandresortegon@gmail.com>
2021-05-26 09:49:58 +0200

    move BooksForm and BookList component to containers directory

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
-import booksReducer from './reducers/books';
+import booksReducer from './reducers/index';
 
 const defaultState = {
   firstBook: {
