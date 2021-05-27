1c58d09ca49092a21c15d4d483c50ac6bdad3ab2
AshakaE <ashakaegerega@gmail.com>
2021-05-27 14:37:55 +0100

    Add style for background and container

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
 import '../App.css';
 import '../index.css';
 
 const App = () => (
-  <div className="panel-bg center">
-    <div className="panel-head">
+  <div className="panel-bg m-auto max-h-[85vh] mt-14 shadow-2xl">
+    <div className="panel-head border-b-2 border-gray-100">
       <div className="flex items-center justify-between">
         <div className="flex items-center">
           <h1 className="Bookstore-CMS">Bookstore CMS</h1>
           <p className="BOOKS uppercase">Books</p>
           <p className="CATEGORIES uppercase">Categories</p>
         </div>
-        <div className="Oval">
-          <div className="Mask">&nbsp;</div>
+        <div className="Oval rounded-full flex">
+          <FontAwesomeIcon icon={faUser} className="Mask" />
+          {/* <div className="Mask">&</div> */}
         </div>
       </div>
     </div>
