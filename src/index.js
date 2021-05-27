60e28da2f914f30370942614ce95ad9f2400626c
helman101 <helmanandresortegon@gmail.com>
2021-05-27 11:33:58 +0200

    delete index.css import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import App from './components/App';
