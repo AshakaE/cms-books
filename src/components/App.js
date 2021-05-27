7b59b4a8f3ce925ef84df816a16815aa745bd359
AshakaE <ashakaegerega@gmail.com>
2021-05-27 11:18:18 +0100

    Create background styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
+import '../App.css';
 
 const App = () => (
-  <div>
+  <div className="panel-bg center">
+    <div className="panel-head">
+      <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+      <p>Books</p>
+      <p>Categories</p>
+    </div>
     <BooksList />
     <BooksForm />
   </div>
