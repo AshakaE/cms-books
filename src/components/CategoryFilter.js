2da8b90d95093a0abf05039f86ffcd25792ecdc2
AshakaE <ashakaegerega@gmail.com>
2021-05-27 08:51:40 +0100

    Implement category filter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CategoryFilter = () => (
+  <select name='filterCategory' id='filterCategory'>
+    <option value='All'>all</option>
+  </select>
+);
+
+export default CategoryFilter;
